e5b573d8ebe09e609c0926ffd7bb702d525bae9a
Swed0ua <shmagala2001@gmail.com>
2023-07-20 03:51:27 +0300

    push work full design and func

--- scripts.js
+++ scripts.js
@@ -1,3 +1,34 @@
 let tg = window.Telegram.WebApp;
 let btn_save = document.querySelector('.btn__save')
-let btn_cancel = document.querySelectorAll('.btn__cancel')
+let btns_cancel = document.querySelectorAll('.btn__cancel')
+
+const init_params = () => {
+    let link__input = document.querySelector('#link__input').value,
+    com__input = document.querySelector('#com__input').value,
+    hot_pos = document.querySelector('#hot_pos').value,
+    work_time = document.querySelector('#work_time').value
+}
+
+init_params()
+
+
+const cancel_app = () => {
+    tg.close()
+}
+
+const give_params = () => {
+    tg.sendData(JSON.stringify({
+        "link" : link__input,
+        'com_count' : com__input,
+        'hot_pos' : hot_pos,
+        'time' : work_time
+    }))
+    cancel_app()
+}
+
+
+btn_save.addEventListener('click', give_params)
+
+btns_cancel.forEach(btn_cancel => {
+    btn_cancel.addEventListener('click', cancel_app) 
+});
