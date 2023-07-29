824ca6c401f34087faf8a32ebe81790d5d78fabb
Swed0ua <shmagala2001@gmail.com>
2023-07-29 15:48:21 +0300

    add input speed/h and hide inputs: hot_pos, com_input

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 let tg = window.Telegram.WebApp;
 let btn_save = document.querySelector('.btn__save')
 let btns_cancel = document.querySelectorAll('.btn__cancel')
-let link__input, com__input, upv_input, hot_pos, work_time
+let link__input, com__input, upv_input, hot_pos, work_time, speed_h
 const init_params = () => {
     link__input = document.querySelector('#link__input').value,
-    com__input = document.querySelector('#com__input').value,
     upv_input = document.querySelector('#upv__input').value,
-    hot_pos = document.querySelector('#hot_pos').value,
+    // com__input = document.querySelector('#com__input').value,
+    // hot_pos = document.querySelector('#hot_pos').value,
     work_time = document.querySelector('#work_time').value
+    speed_h = document.querySelector('#speed_h').value
 }
 
 init_params()
@@ -21,9 +22,10 @@ const give_params = () => {
     init_params()
     tg.sendData(JSON.stringify({
         "link" : link__input,
-        'com_count' : com__input,
         'upv_count': upv_input,
-        'hot_pos' : hot_pos,
+        // 'com_count' : com__input,
+        // 'hot_pos' : hot_pos,
+        'speed_h': speed_h,
         'time' : work_time
     }))
     cancel_app()
